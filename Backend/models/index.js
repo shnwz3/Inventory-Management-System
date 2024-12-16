9cd0fe2db35b8c2a4dad04afde3f4218c0eb99aa
shnwz3 <mr.shahnawaz03@gmail.com>
2024-12-16 15:58:16 +0530

    .

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://adminhamza:adminhamza123&@cluster0.pzcviot.mongodb.net/InventoryManagementApp?retryWrites=true&w=majority";
 
+// Load environment variables
+require("dotenv").config();
+
+// Access the MongoDB URI from the environment variables
+const uri = process.env.DATABASE_URL;
 
 function main() {
     mongoose.connect(uri).then(() => {
-        console.log("Succesfull")
-    
+        console.log("Successfully connected to MongoDB");
     }).catch((err) => {
-        console.log("Error: ", err)
-    })
+        console.log("Error connecting to MongoDB: ", err);
+    });
 }
 
 module.exports = { main };
